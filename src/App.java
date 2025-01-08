3a648a9587bdce3cf2329a62961a0d4cbc339ccd
Eleonora27-mon <monterisieleonora@gmail.com>
2025-01-08 13:01:41 +0100

    fase 5: incompleta!

--- src/App.java
+++ src/App.java
@@ -8,34 +8,58 @@ public class App {
 
         int speedLim, yourSpeed, eccesso, multa;
         String plate;
+        String ancora;
 
-        System.out.println("Inserisci il limite di velocità: ");
-        speedLim = sc.nextInt();
-        sc.nextLine();
+    do { System.out.println("Inserisci il limite di velocità: ");
+    speedLim = sc.nextInt();
+    sc.nextLine();
 
-        System.out.println("La tua targa: ");
-        plate = sc.nextLine();
+    System.out.println("La tua targa: ");
+    plate = sc.nextLine();
 
-        System.out.println("La tua velocità: ");
-        yourSpeed = sc.nextInt();
-        sc.nextLine();
+    System.out.println("La tua velocità: ");
+    yourSpeed = sc.nextInt();
+    sc.nextLine();
 
-        Map<Integer,Integer> sanzioni = new LinkedHashMap<>();
-        sanzioni.put(5, 60);
-        sanzioni.put(10, 200);
-        sanzioni.put(30, 500);
-        sanzioni.put(50, 1000);
+    Map<Integer,Integer> sanzioni = new LinkedHashMap<>();
+    sanzioni.put(5, 0);
+    sanzioni.put(10, 60);
+    sanzioni.put(30, 200);
+    sanzioni.put(50, 500);
+    sanzioni.put(999, 1000);
 
-        eccesso = yourSpeed - speedLim;
-
-        for (Integer n : sanzioni.keySet()) {
-            if (n > eccesso) {
-                multa = sanzioni.get(n);
-                System.out.println("La tua multa è di " + multa + " euro.");
-                break;
-            }
+    eccesso = yourSpeed - speedLim;
+    Map<String,Integer> importo = new LinkedHashMap<>();
+    for (Integer n : sanzioni.keySet()) {
+        if (n >=eccesso) {
+            multa = sanzioni.get(n);
+            System.out.println("La tua multa è di " + multa + " euro.");
             
+            importo.put(plate, multa);
+            
+
+            break;
+            
+
+        
         }
+        
+    }
+    System.out.println(importo.toString());
+    System.out.println("Vuoi effettuare una nuova rilevazione? s/n");
+    ancora=sc.nextLine();
+
+
+        
+    } while (ancora.equalsIgnoreCase ("s"));
+
+
+
+    System.out.println("Rilevazione terminata!");
+    
+
+
+
         
 
         sc.close();
